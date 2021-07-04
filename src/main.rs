8d3973372674fa2b81d98682c706d619036b9579
juryokun <capeta.kozo@gmail.com>
2021-07-04 20:59:14 +0900

    2021/7/4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{stdout, BufReader, BufWriter, Write};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -6,13 +9,20 @@ struct Cli {
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let reader = BufReader::new(File::open(&args.path)?);
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println! {"{}", line};
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+
+    for line in reader.lines() {
+        let content = line?;
+        if content.contains(&args.pattern) {
+            // println! {"{}", content};
+            writeln!(out, "{}", content)?;
         }
     }
+    Ok(())
 }
