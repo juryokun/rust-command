51f30f2bd37eafa14f3bc50d54bce74a427894e3
juryokun <capeta.kozo@gmail.com>
2021-07-18 23:12:58 +0900

    2021/7/18

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use env_logger;
+use log::{info, warn};
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{stdout, BufReader, BufWriter, Write};
@@ -10,8 +13,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+    info!("starting up");
+    info!("warn");
+
     let args = Cli::from_args();
-    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
     let reader = BufReader::new(File::open(&args.path)?);
 
     let out = stdout();
