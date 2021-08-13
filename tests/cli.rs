a90458f7b61bd77741bb34c3a745f8390d539757
juryokun <capeta.kozo@gmail.com>
2021-08-13 18:32:33 +0900

    2021/8/13

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,6 +2,9 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
+use std::io::{self, Write};
+use tempfile::NamedTempFile;
+
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("rust-command")?;
@@ -10,5 +13,19 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("rust-command")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("test\nAnother test"));
+
     Ok(())
 }
